84aab0f61e7319dd19748d4149012c21460f507d
Bence Magyar <bence.magyar@pal-robotics.com>
2014-01-22 18:09:14 +0100

    Added calling block_grasp_generator

--- scripts/grasp_object_server.py
+++ scripts/grasp_object_server.py
@@ -37,18 +37,19 @@
 import rospy
 import tf
 from actionlib import ActionServer, SimpleActionClient
-from actionlib import ServerGoalHandle
+from geometry_msgs.msg import PoseArray, Pose
+from std_msgs.msg import Header
 #own imports
 from reem_tabletop_grasping.msg import GraspObjectAction 
 from reem_tabletop_grasping.msg import GraspObjectFeedback
 #perception imports & grasp planning imports
 from object_recognition_msgs.msg import RecognizedObjectArray
 from object_recognition_clusters import ClusterBoundingBoxFinder
-from block_grasp_generator.msg import GenerateBlockGraspsAction, GenerateBlockGraspsGoal, GenerateBlockGraspsResult
+from block_grasp_generator.msg import GenerateBlockGraspsAction, GenerateBlockGraspsGoal
 #manipulation imports
 from moveit_msgs.msg import Grasp, PickupAction, PickupGoal, PickupResult, GripperTranslation, MoveItErrorCodes
 from moveit_commander import RobotCommander, PlanningSceneInterface
-from reem_tabletop_grasping.msg._GraspObjectActionGoal import GraspObjectActionGoal
+
 
 class GraspObjectServer:
 
@@ -56,10 +57,11 @@ class GraspObjectServer:
         # stuff for grasp planning
         self.tf_listener = tf.TransformListener()
         self.tf_broadcaster = tf.TransformBroadcaster()
-        self.cbbf = ClusterBoundingBoxFinder(self.tf_listener, self.tf_broadcaster)
+        self.cbbf = ClusterBoundingBoxFinder(self.tf_listener, self.tf_broadcaster, "base_link")
         self.last_objects = RecognizedObjectArray()
         #rospy.Subscriber("object_array", RecognizedObjectArray, self.objects_callback)
-        rospy.Subscriber("/recognized_object_array", RecognizedObjectArray, self.objects_callback)
+        self.sub = rospy.Subscriber("/recognized_object_array", RecognizedObjectArray, self.objects_callback)
+        self.grasp_publisher = rospy.Publisher("grasp_pose_from_block_bla", PoseArray)
         
         rospy.loginfo("Connecting to pickup AS")
         self.pickup_ac = SimpleActionClient('/pickup', PickupAction)
@@ -78,6 +80,7 @@ class GraspObjectServer:
 
     def objects_callback(self, data):
         rospy.loginfo(rospy.get_name() + ": This message contains %d objects." % len(data.objects))
+        #rospy.loginfo(data.objects[0].point_clouds[0].header.frame_id)
         self.last_objects = data
         
     def goal_callback(self, goal):      
@@ -103,22 +106,59 @@ class GraspObjectServer:
     def grasping_sm(self):
       if self.current_goal:
         self.update_feedback("Running clustering")
-        #self.cbbf.find_object_frame_and_bounding_box(self.last_objects)
-        #self.current_goal.set_aborted()
-        rospy.sleep(1.0)
+        (object_points, object_bounding_box_dims, 
+         object_bounding_box, object_pose) = self.cbbf.find_object_frame_and_bounding_box(self.last_objects.objects[self.current_goal.get_goal().target_id].point_clouds[0])
+        #TODO visualize bbox
+        print object_bounding_box_dims
         self.update_feedback("check reachability")
         rospy.sleep(1.0)
         self.update_feedback("generate grasps")
-        rospy.sleep(1.0)
+        # pose + width is the bbox size on x
+        grasp_list = self.generate_grasps(object_pose, object_bounding_box_dims[0])
+        self.publish_grasps_as_poses(grasp_list)
         self.update_feedback("setup planning scene")
         rospy.sleep(1.0)
         self.update_feedback("execute grasps")
         rospy.sleep(1.0)
         self.current_goal.set_succeeded()
+        #self.current_goal.set_aborted()
         
     def update_feedback(self, text):
         self.feedback.last_state = text
         self.current_goal.publish_feedback(self.feedback)
+        
+    def generate_grasps(self, pose, width):
+          self.grasps_ac.wait_for_server()
+          rospy.loginfo("Succesfully connected.")
+          goal = GenerateBlockGraspsGoal()
+          goal.pose = pose.pose
+          goal.width = width
+          self.grasps_ac.send_goal(goal)
+          rospy.loginfo("Sent goal, waiting:\n" + str(goal))
+          t_start = rospy.Time.now()
+          self.grasps_ac.wait_for_result()
+          t_end = rospy.Time.now()
+          t_total = t_end - t_start
+          rospy.loginfo("Result received in " + str(t_total.to_sec()))
+          grasp_list = self.grasps_ac.get_result().grasps
+          return grasp_list
+
+    def publish_grasps_as_poses(self, grasps):
+          rospy.loginfo("Publishing PoseArray on /grasp_pose_from_block_bla for grasp_pose")
+          graspmsg = Grasp()
+          grasp_PA = PoseArray()
+          header = Header()
+          header.frame_id = "base_link"
+          header.stamp = rospy.Time.now()
+          grasp_PA.header = header
+          for graspmsg in grasps:
+              #print graspmsg
+              #print type(graspmsg)
+              p = Pose(graspmsg.grasp_pose.pose.position, graspmsg.grasp_pose.pose.orientation)
+              grasp_PA.poses.append(p)
+          self.grasp_publisher.publish(grasp_PA)
+          rospy.sleep(0.1)
+
 
 if __name__ == '__main__':
     name = 'grasp_object_server'
