d66c849bad6da1f6e208d1d69c7132c7f3926b4f
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2014-04-05 19:39:03 +0200

    First version of the AS to grasp anything in a pose. that you can send a PoseStamped You can send a PoseStamped and it will grasp the cluster detected by the clustering using tabletop to filter tables closer to that pose.

--- scripts/grasp_object_server.py
+++ scripts/grasp_object_server.py
@@ -37,8 +37,9 @@
 import rospy
 import tf
 from actionlib import ActionServer, SimpleActionClient
-from geometry_msgs.msg import PoseArray, Pose, Point
+from geometry_msgs.msg import PoseArray, Pose, Point, PoseStamped
 from std_msgs.msg import Header
+from std_srvs.srv import Empty, EmptyRequest
 #own imports
 from reem_tabletop_grasping.msg import GraspObjectAction 
 from reem_tabletop_grasping.msg import GraspObjectFeedback
@@ -51,6 +52,9 @@ from moveit_msgs.msg import Grasp, PickupAction, PickupGoal, PickupResult, Gripp
 from moveit_commander import RobotCommander, PlanningSceneInterface
 from reem_tabletop_grasping.msg._GraspObjectResult import GraspObjectResult
 import geometry_msgs
+#other imports
+import copy
+import numpy as np
 
 moveit_error_dict = {}
 for name in MoveItErrorCodes.__dict__.keys():
@@ -63,21 +67,29 @@ class GraspObjectServer:
 
     def __init__(self, name):
         # stuff for grasp planning
+        rospy.loginfo("Getting a TransformListener...")
         self.tf_listener = tf.TransformListener()
+        rospy.loginfo("Getting a TransformBroadcaster...")
         self.tf_broadcaster = tf.TransformBroadcaster()
+        rospy.loginfo("Initializing a ClusterBoundingBoxFinder...")
         self.cbbf = ClusterBoundingBoxFinder(self.tf_listener, self.tf_broadcaster, "base_link")
-        self.last_objects = RecognizedObjectArray()
-        #rospy.Subscriber("object_array", RecognizedObjectArray, self.objects_callback)
+        self.last_clusters = None
         self.sub = rospy.Subscriber("/recognized_object_array", RecognizedObjectArray, self.objects_callback)
         self.grasp_publisher = rospy.Publisher("generated_grasps", PoseArray)
         
+        self.to_grasp_object_pose_pub = rospy.Publisher("/to_grasp_object_pose", PoseStamped)
+        
         rospy.loginfo("Connecting to pickup AS")
         self.pickup_ac = SimpleActionClient('/pickup', PickupAction)
-        #pickup_ac.wait_for_server() # needed?
+        self.pickup_ac.wait_for_server() 
         
         rospy.loginfo("Connecting to grasp generator AS")
         self.grasps_ac = SimpleActionClient('/grasp_generator_server/generate', GenerateBlockGraspsAction)
-        #grasps_ac.wait_for_server() # needed? 
+        self.grasps_ac.wait_for_server()
+        
+        rospy.loginfo("Connecting to depth throttle server...")
+        self.depth_service = rospy.ServiceProxy('/depth_throtle', Empty)
+        self.depth_service.wait_for_service()
         
         #planning scene for motion planning
         self.scene = PlanningSceneInterface()
@@ -96,15 +108,16 @@ class GraspObjectServer:
 
     def objects_callback(self, data):
         rospy.loginfo(rospy.get_name() + ": This message contains %d objects." % len(data.objects))
-        self.last_objects = data
+        self.last_clusters = data
         
     def goal_callback(self, goal):      
         if self.current_goal:
             goal.set_rejected() # "Server busy"
             return
-        elif len(self.last_objects.objects) - 1 < goal.get_goal().target_id:
-            goal.set_rejected() # "No objects to grasp were received on the objects topic."
-            return
+        # TODO: Check if pose is not empty, if it is, reject
+#         elif len(self.last_clusters.objects) - 1 < goal.get_goal().target_id:
+#             goal.set_rejected() # "No objects to grasp were received on the objects topic."
+#             return
         else:
             #store and accept new goal
             self.current_goal = goal
@@ -120,18 +133,36 @@ class GraspObjectServer:
 
     def grasping_sm(self):
         if self.current_goal:
-            self.update_feedback("running clustering")
-            (object_points, obj_bbox_dims, 
-             object_bounding_box, object_pose) = self.cbbf.find_object_frame_and_bounding_box(self.last_objects.objects[self.current_goal.get_goal().target_id].point_clouds[0])
+            goal_message_field = self.current_goal.get_goal()
+            # Publish pose of goal position
+            self.to_grasp_object_pose_pub.publish(goal_message_field.pose_of_cluster)
+            self.update_feedback("Detecting clusters")
+            if not self.wait_for_recognized_array(wait_time=5, timeout_time=10): # wait until we get clusters published
+                self.update_aborted("Failed detecting clusters")
+            
+            # Search closer cluster
+            # transform pose to base_link if needed
+            if goal_message_field.pose_of_cluster.header.frame_id != "base_link":
+                self.tf_listener.waitForTransform("base_link", goal_message_field.pose_of_cluster.header.frame_id, goal_message_field.pose_of_cluster.header.stamp, rospy.Duration(5))
+                object_to_grasp_pose =  self.tf_listener.transformPose("base_link", goal_message_field.pose_of_cluster)
+            else:
+                object_to_grasp_pose = goal_message_field.pose_of_cluster.pose
+            
+            
+            self.update_feedback("Searching closer cluster while clustering")
+            (closest_cluster_id, (object_points, obj_bbox_dims, object_bounding_box, object_pose)) = self.get_id_of_closest_cluster_to_pose(object_to_grasp_pose) 
+            
+            rospy.logwarn("in AS: Closest cluster id is: " + str(closest_cluster_id))
             #TODO visualize bbox
             #TODO publish filtered pointcloud?
-            print obj_bbox_dims
+            rospy.loginfo("BBOX: " + str(obj_bbox_dims))
             ########
             self.update_feedback("check reachability")
             ########
             self.update_feedback("generate grasps")
-            #transform pose to base_link
-            self.tf_listener.waitForTransform("base_link", object_pose.header.frame_id, object_pose.header.stamp, rospy.Duration(5))
+            rospy.logwarn("Object pose before tf thing is: " + str(object_pose))
+            #transform pose to base_link, IS THIS NECESSARY?? should be a function in any case
+            self.tf_listener.waitForTransform("base_link", object_pose.header.frame_id, object_pose.header.stamp, rospy.Duration(15))
             trans_pose = self.tf_listener.transformPose("base_link", object_pose)
             object_pose = trans_pose
             #HACK remove orientation -> pose is aligned with parent(base_link)
@@ -235,9 +266,87 @@ class GraspObjectServer:
         pug.planning_options.replan = True
         pug.planning_options.replan_attempts = 10
         #pug.attached_object_touch_links = ['arm_right_5_link', "hand_right_grasping_frame"]
-        pug.allowed_touch_objects.append(target)
+        #pug.allowed_touch_objects.append(target)
         #pug.attached_object_touch_links.append('all')  
         return pug
+
+
+    def dist_between_poses(self, pose1, pose2):
+        """Calculates the distance between two points in euclidean space using numpy
+        poses can be Pose or PoseStamped"""
+        if type(pose1) == type(Pose()):
+            p1 = np.array(pose1.position.__getstate__())
+        elif type(pose1) == type(PoseStamped()):
+            p1 = np.array(pose1.pose.position.__getstate__())
+            
+        if type(pose2) == type(Pose()):
+            p2 = np.array(pose2.position.__getstate__())
+        elif type(pose2) == type(PoseStamped()):
+            p2 = np.array(pose2.pose.position.__getstate__())
+            
+        dist = np.linalg.norm(p1-p2, ord=3)
+        return dist
+        
+        
+    def get_id_of_closest_cluster_to_pose(self, input_pose):
+        """Returns the id of the closest cluster to the pose provided (in Pose or PoseStamped) 
+        and all the associated clustering information"""
+        current_id = 0
+        closest_pose = None
+        closest_object_points = None
+        closest_id = 0
+        closest_bbox = None
+        closest_bbox_dims = None
+        closest_distance = 99999.9
+        for myobject in self.last_clusters.objects:  
+            (object_points, obj_bbox_dims, object_bounding_box, object_pose) = self.cbbf.find_object_frame_and_bounding_box(myobject.point_clouds[0])
+            self.tf_listener.waitForTransform("base_link", object_pose.header.frame_id, object_pose.header.stamp, rospy.Duration(15))
+            trans_pose = self.tf_listener.transformPose("base_link", object_pose)
+            object_pose = trans_pose
+            rospy.loginfo("id: " + str(current_id) + "\n pose:\n" + str(object_pose))
+            if closest_pose == None: # first loop iteration
+                closest_object_points = object_points
+                closest_pose = object_pose
+                closest_bbox = object_bounding_box
+                closest_bbox_dims = obj_bbox_dims
+                closest_distance = self.dist_between_poses(closest_pose, input_pose)
+            else:
+                if self.dist_between_poses(object_pose, input_pose) < closest_distance:
+                    closest_object_points = object_points
+                    closest_distance = self.dist_between_poses(object_pose, input_pose)
+                    closest_pose = object_pose
+                    closest_bbox = object_bounding_box
+                    closest_bbox_dims = obj_bbox_dims
+                    closest_id = current_id
+            current_id+=1
+        rospy.loginfo("Closest id is: " + str(closest_id) + " at " + str(closest_pose))
+        return closest_id, (closest_object_points, closest_bbox_dims, closest_bbox_dims, closest_pose)
+    
+    
+    def wait_for_recognized_array(self, wait_time=6, timeout_time=10):
+        """Ask for depth images until we get a recognized array
+        wait for wait_time between depth throtle calls
+        stop if timeout_time is achieved
+        If we dont find it in the correspondent time return false, true otherwise"""
+        initial_time = rospy.Time.now()
+        self.last_clusters = None
+        count = 0
+        num_calls = 1
+        self.depth_service.call(EmptyRequest())
+        rospy.loginfo("Depth throtle server call #" + str(num_calls))
+        rospy.loginfo("Waiting for a recognized array...")
+        while rospy.Time.now() - initial_time < rospy.Duration(timeout_time) and self.last_clusters == None:
+            rospy.sleep(0.1)
+            count += 1
+            if count >= wait_time/10:
+                rospy.loginfo("Depth throtle server call #" + str(num_calls))
+                self.depth_service.call(EmptyRequest())
+        if self.last_clusters == None:
+            return False
+        else:
+            return True
+    
+
 
 
 if __name__ == '__main__':
